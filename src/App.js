21aa5ce6994a7b393c5e4218588566a7992ed6a5
Amirali Soltani <97620073+AmiraliSoltani@users.noreply.github.com>
2023-10-18 02:14:50 -0400

    test the github

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>hi , this is Amirali</p>
+        <p>hi , this is Amirali , just for test</p>
       </header>
     </div>
   );
