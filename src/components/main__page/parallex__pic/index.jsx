a03720a286cc9f0c50ebb998df0c3a3bf8602c35
Amirali Soltani <asoltani7@myseneca.ca>
2024-10-12 13:01:41 -0700

    responsive day 3!

--- src/components/main__page/parallex__pic/index.jsx
+++ src/components/main__page/parallex__pic/index.jsx
@@ -6,8 +6,8 @@ function ParallexPic() {
     <div className="container-special">
       <div className="parallex__background__image">
         <div className="text__in__parallex">
-          <div className="top__text">Buy winter clothing now with a discount</div>
-          <div className="buttom__text">Buy now</div>
+        <div className="top__text">Get exclusive discounts on winter clothing today</div>
+        <div className="buttom__text">Buy now</div>
         </div>
       </div>
     </div>
