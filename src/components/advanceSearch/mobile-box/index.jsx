d8289c4a0611172806f7bce2e67670a6d7bad44f
Amirali Soltani <asoltani7@myseneca.ca>
2024-10-17 09:12:38 -0700

    responsive day 8!

--- src/components/advanceSearch/mobile-box/index.jsx
+++ src/components/advanceSearch/mobile-box/index.jsx
@@ -5,10 +5,14 @@ const MobileBox = () => {
   return(
     <div className="mobile_box ">
         <div className="sort">
+        <img src={require("./../../../assets/icons/sort.png")} alt="sort" />
+
 <span>Sort</span>
+
         </div>
 
         <div className="filter">
+            <img src={require("./../../../assets/icons/filter-mobile-2.png")} alt="sort" />
             <span>Filter</span>
 
         </div>
