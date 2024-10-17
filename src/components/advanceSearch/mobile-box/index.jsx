f1bf6f47ac59848fade47a5e4f7c62ff45aa92ed
Amirali Soltani <asoltani7@myseneca.ca>
2024-10-17 15:55:29 -0700

    responsive day 8!

--- src/components/advanceSearch/mobile-box/index.jsx
+++ src/components/advanceSearch/mobile-box/index.jsx
@@ -8,59 +8,45 @@ const MobileBox = () => {
         infinite: false,
         speed: 500,
         slidesToScroll: 1,
-        //slidesToShow: 4,
         variableWidth: true, // Allow slides to adjust to their content width
-    
-      };
+    };
 
-  return(
-    <div className="mobile_box ">
-        <div className="sort">
-        <img src={require("./../../../assets/icons/sort.png")} alt="sort" />
+    return(
+        <div className="mobile_box">
+            <div className="sort">
+                <img src={require("./../../../assets/icons/sort.png")} alt="sort" />
+                <span>Sort</span>
+            </div>
 
-<span>Sort</span>
+            <div className="filter">
+                <img src={require("./../../../assets/icons/filter-mobile-2.png")} alt="filter" />
+                <span>Filter</span>
+            </div>
 
+            <div className="slider-container"> {/* Wrapper for the slider */}
+                <Slider {...settings2}>
+                    <div className='one-filter'>
+                        <span><img src={require("./../../../assets/icons/down.png")} alt="sort" /> Categories</span>
+                    </div>
+                    <div className='one-filter'>
+                        <span>only Discounted</span>
+                    </div>
+                    <div className='one-filter'>
+                        <span><img src={require("./../../../assets/icons/down.png")} alt="sort" /> Price Range</span>
+                    </div>
+                    <div className='one-filter'>
+                        <span><img src={require("./../../../assets/icons/down.png")} alt="sort" /> Color</span>
+                    </div>
+                    <div className='one-filter'>
+                        <span><img src={require("./../../../assets/icons/down.png")} alt="sort" /> Size</span>
+                    </div>
+                    <div className='one-filter'>
+                        <span><img src={require("./../../../assets/icons/down.png")} alt="sort" /> Brand</span>
+                    </div>
+                </Slider>
+            </div>
         </div>
-
-        <div className="filter">
-            <img src={require("./../../../assets/icons/filter-mobile-2.png")} alt="sort" />
-            <span>Filter</span>
-
-        </div>
-        {/* <div className="all"> */}
-        <Slider {...settings2}>
-           <div className='one-filter'>
-            <span>
-            <img src={require("./../../../assets/icons/down.png")} alt="sort" />
-            Categories</span>
-           </div> 
-           <div className='one-filter'>
-             <span>
-                only Discounted
-                 </span>
-             </div> 
-           <div className='one-filter'>
-              <span>
-              <img src={require("./../../../assets/icons/down.png")} alt="sort" />
-              Price Range</span>
-              </div> 
-           <div className='one-filter'>  <span>
-           <img src={require("./../../../assets/icons/down.png")} alt="sort" />
-            Color</span></div> 
-           <div className='one-filter'>  <span>
-           <img src={require("./../../../assets/icons/down.png")} alt="sort" />
-            Size</span></div> 
-           <div className='one-filter'>  <span>
-           <img src={require("./../../../assets/icons/down.png")} alt="sort" />
-            Brand</span></div> 
-
-
-
-                                  </Slider>
-                                  {/* </div> */}
-
-</div>
-  )
+    );
 };
 
 export default MobileBox;
