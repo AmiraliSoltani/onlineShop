532b0f5937957436926741feae70733567f7cb90
Amirali Soltani <asoltani7@myseneca.ca>
2024-10-17 11:30:54 -0700

    responsive day 8!

--- src/components/advanceSearch/mobile-box/index.jsx
+++ src/components/advanceSearch/mobile-box/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import "../../../css/mobile-box.css"
+import Slider from 'react-slick';
 
 const MobileBox = () => {
+    const settings2 = {
+        ltr: true,
+        infinite: false,
+        speed: 500,
+        slidesToScroll: 1,
+        //slidesToShow: 4,
+        variableWidth: true, // Allow slides to adjust to their content width
+    
+      };
+
   return(
     <div className="mobile_box ">
         <div className="sort">
@@ -16,6 +27,22 @@ const MobileBox = () => {
             <span>Filter</span>
 
         </div>
+        <div className="all">
+        <Slider {...settings2}>
+           <div className='one-filter'>
+            <span>Categories</span>
+           </div> 
+           <div className='one-filter'> <span>only Discounted </span></div> 
+           <div className='one-filter'>  <span>Price Range</span></div> 
+           <div className='one-filter'>  <span>Color</span></div> 
+           <div className='one-filter'>  <span>Size</span></div> 
+           <div className='one-filter'>  <span>Brand</span></div> 
+
+
+
+                                  </Slider>
+                                  </div>
+
 </div>
   )
 };
